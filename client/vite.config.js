59f5a444e84a096e708ca1b1e4ddcb7c020e594c
sarathsanthoshd <sarathsanthoshd@gmail.com>
2025-08-22 22:38:48 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // This forwards any request starting with /api to your backend server
-      '/api': {
-        target: 'https://myportfolio-myud.onrender.com', // Your backend server URL
-        changeOrigin: true,
-      },
-    },
-  },
-});
+})
